885fd64d44cc11121dc0c4532119410caa4edf65
monocoto3000 <223238@ids.upchiapas.edu.mx>
2024-04-17 05:51:58 -0600

    moncambios comentar links para que no se chispe

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,17 +4,24 @@ import { useState } from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
+import { Card, CardContent, Typography, TextField } from '@mui/material';
 import { Metrics } from '@/components/dashboard/overview/metrics-card';
 import { DownloadDocs } from '@/components/dashboard/overview/download-doc';
 import { MetricsCharts } from '@/components/dashboard/overview/chart';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 import { Drop } from '@phosphor-icons/react/dist/ssr/Drop';
 import { Thermometer } from '@phosphor-icons/react/dist/ssr/Thermometer';
 import { Radioactive } from '@phosphor-icons/react/dist/ssr/Radioactive';
+import { io } from 'socket.io-client';
+
 
 const data = [
   {
-    stationName: "Monica",
+    stationId: "Monica",
     temperature: { currentData: 20, max: 35, min: 15 },
     humidity: { currentData: 70, max: 79, min: 65 },
     radiation: { currentData: 23, max: 42, min: 15 },
@@ -23,107 +30,166 @@ const data = [
       humidity: [18, 16, 5, 8, 3, 14, 22, 16, 17, 19, 18, 20],
       radiation: [18, 16, 5, 8, 3, 14, 14, 16, 17, 19, 18, 20]
     }
-  },
-  {
-    stationName: "Cesar",
-    temperature: { currentData: 32, max: 37, min: 13 },
-    humidity: { currentData: 74, max: 87, min: 33 },
-    radiation: { currentData: 34, max: 62, min: 12 },
-    chartsData: {
-      temperature: [22, 24, 18, 19, 20, 22, 25, 28, 29, 31, 32, 33],
-      humidity: [62, 64, 63, 65, 68, 70, 72, 74, 76, 78, 80, 82],
-      radiation: [28, 30, 32, 33, 34, 35, 36, 38, 40, 42, 44, 45]
-    }
   }
 ];
 
 export default function Page(): React.JSX.Element {
+
+  const baseURL = 'http://localhost:8080/paciente/getAll';
+  const [data, setData] = useState([]);
+  const [rtdata, setRtdata] = useState(null);
+  const [average, setAverage] = useState(null);
+
+
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    axios.post(baseURL, {
+      params: {
+        "stationId": "1"
+      }
+    })
+      .then(response => {
+        if (response != null) {
+          setData(response?.data);
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }, []);
+
+  const socket = io('http://localhost:4000');
+
+  socket.on('rtdata', (data: any) => {
+    console.log(data)
+    setRtdata(data)
+  });
+
+  socket.on('averages', (data: any) => {
+    console.log(data)
+    setAverage(data)
+  });
+
   const [selectedTab, setSelectedTab] = React.useState(0);
+  const [selectedStation, setSelectedStation] = useState(null);
+  const [showDashboard, setShowDashboard] = useState(false);
 
   const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
     setSelectedTab(newValue);
   };
 
+  const handleAccept = () => {
+    if (selectedStation !== null) {
+      setShowDashboard(true);
+    }
+  };
 
   return (
     <Grid container spacing={2}>
-      <Grid container xs={12}>
-        <Tabs value={selectedTab} onChange={handleChangeTab} indicatorColor="secondary">
-          {data.map((station, index) => (
-            <Tab key={index} label={station.stationName} />
-          ))}
-        </Tabs>
-      </Grid>
-      <Grid container lg={4} spacing={2} direction={'column'}>
-        <Grid lg={12} xs={12}>
-          <Metrics
-            dataType="Temperatura"
-            currentData={data[selectedTab].temperature.currentData}
-            value="°C"
-            max={data[selectedTab].temperature.max}
-            min={data[selectedTab].temperature.min}
-            icon={<Thermometer size={32} />}
-          />
+      {!showDashboard && (
+        <Grid container lg={12} justifyContent="center">
+          <Grid item lg={6} xs={12}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5" component="h2" style={{ margin: 10 }}>
+                  Seleccione una estación
+                </Typography>
+                <Autocomplete
+                  style={{ margin: 10 }}
+                  options={data.map(station => station.stationId)}
+                  onChange={(event, value) => setSelectedStation(value)}
+                  renderInput={(params) => <TextField {...params} label="Estación" />}
+                />
+                <Button variant="contained" onClick={handleAccept} fullWidth>Aceptar</Button>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid lg={12} xs={12}>
-          <Metrics
-            dataType="Humedad"
-            currentData={data[selectedTab].humidity.currentData}
-            value="%"
-            max={data[selectedTab].humidity.max}
-            min={data[selectedTab].humidity.min}
-            icon={<Drop size={32} />}
-          />
-        </Grid>
-        <Grid lg={12} xs={12}>
-          <Metrics
-            dataType="Radación"
-            currentData={data[selectedTab].radiation.currentData}
-            value="V"
-            max={data[selectedTab].radiation.max}
-            min={data[selectedTab].radiation.min}
-            icon={<Radioactive size={32} />}
-          />
-        </Grid>
-        <Grid lg={12} xs={12}>
-          <DownloadDocs />
-        </Grid>
-      </Grid>
-      <Grid container lg={8} spacing={2}>
-        <Grid lg={12} xs={12}>
-          <MetricsCharts
-            chartSeries={[
-              { name: 'Humedad', data: data[selectedTab].chartsData.temperature }
-            ]}
-            sx={{ height: '100%' }}
-            title="Temperatura"
-            metric="°C"
-            color="#FF953D"
-          />
-        </Grid>
-        <Grid lg={12} xs={12}>
-          <MetricsCharts
-            chartSeries={[
-              { name: 'Humedad', data: data[selectedTab].chartsData.humidity }
-            ]}
-            sx={{ height: '100%' }}
-            title="Humedad"
-            metric="%"
-            color="#3D84FF"
-          />
-        </Grid>
-        <Grid lg={12} xs={12}>
-          <MetricsCharts
-            chartSeries={[
-              { name: 'Radiación', data: data[selectedTab].chartsData.radiation }
-            ]}
-            sx={{ height: '100%' }}
-            title="Radiación"
-            metric="V"
-            color="#FF403D"
-          />
-        </Grid>
-      </Grid>
+      )}
+      {showDashboard && (
+        <>
+          <Grid container spacing={2}>
+            <Grid container xs={12}>
+              <Tabs value={selectedTab} onChange={handleChangeTab} indicatorColor="secondary">
+                {data.map((station, index) => (
+                  <Tab key={index} label={station.stationId} />
+                ))}
+              </Tabs>
+            </Grid>
+            <Grid container lg={4} spacing={2} direction={'column'}>
+              <Grid lg={12} xs={12}>
+                <Metrics
+                  dataType="Temperatura"
+                  currentData={data[selectedTab].temperature.currentData}
+                  value="°C"
+                  max={data[selectedTab].temperature.max}
+                  min={data[selectedTab].temperature.min}
+                  icon={<Thermometer size={32} />}
+                />
+              </Grid>
+              <Grid lg={12} xs={12}>
+                <Metrics
+                  dataType="Humedad"
+                  currentData={data[selectedTab].humidity.currentData}
+                  value="%"
+                  max={data[selectedTab].humidity.max}
+                  min={data[selectedTab].humidity.min}
+                  icon={<Drop size={32} />}
+                />
+              </Grid>
+              <Grid lg={12} xs={12}>
+                <Metrics
+                  dataType="Radación"
+                  currentData={data[selectedTab].radiation.currentData}
+                  value="V"
+                  max={data[selectedTab].radiation.max}
+                  min={data[selectedTab].radiation.min}
+                  icon={<Radioactive size={32} />}
+                />
+              </Grid>
+              <Grid lg={12} xs={12}>
+                <DownloadDocs />
+              </Grid>
+            </Grid>
+            <Grid container lg={8} spacing={2}>
+              <Grid lg={12} xs={12}>
+                <MetricsCharts
+                  chartSeries={[
+                    { name: 'Humedad', data: data[selectedTab].chartsData.temperature }
+                  ]}
+                  sx={{ height: '100%' }}
+                  title="Temperatura"
+                  metric="°C"
+                  color="#FF953D"
+                />
+              </Grid>
+              <Grid lg={12} xs={12}>
+                <MetricsCharts
+                  chartSeries={[
+                    { name: 'Humedad', data: data[selectedTab].chartsData.humidity }
+                  ]}
+                  sx={{ height: '100%' }}
+                  title="Humedad"
+                  metric="%"
+                  color="#3D84FF"
+                />
+              </Grid>
+              <Grid lg={12} xs={12}>
+                <MetricsCharts
+                  chartSeries={[
+                    { name: 'Radiación', data: data[selectedTab].chartsData.radiation }
+                  ]}
+                  sx={{ height: '100%' }}
+                  title="Radiación"
+                  metric="V"
+                  color="#FF403D"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </>
+      )}
     </Grid>
   );
 }
--- src/components/dashboard/overview/download-doc.tsx
+++ src/components/dashboard/overview/download-doc.tsx
@@ -1,24 +1,58 @@
-'use client';
-
-import * as React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import Grid from '@mui/material/Unstable_Grid2';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-import { Button, CardActions, InputLabel } from '@mui/material';
+import { Button, CardActions, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+
+interface DataItem {
+  temperature: number;
+  humidity: number;
+  radiation: number;
+}
+
+let array: DataItem[] = [
+  { temperature: 20, humidity: 35, radiation: 90 },
+  { temperature: 21, humidity: 36, radiation: 91 },
+  { temperature: 22, humidity: 37, radiation: 92 }
+];
 
 export function DownloadDocs(): React.JSX.Element {
+  const [data, setData] = useState<DataItem[]>([]);
+
+  const handleSearch = () => {
+    setData(array);
+  };
+
   return (
     <Card>
       <CardHeader title="Descargables" subheader="Seleccione un intervalo de fechas para descargar los datos de temperatura, humedad y radiación."/>
       <CardContent>
         <Grid container direction={'column'}>
-            <DateRangePicker localeText={{ start: 'Fecha de inicio', end: 'Fecha final' }}/>
+          <DateRangePicker localeText={{ start: 'Fecha de inicio', end: 'Fecha final' }}/>
         </Grid>
+        <Button onClick={handleSearch}>Buscar</Button>
       </CardContent>
       <CardActions>
-        <Button>Generar documento</Button>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Temperatura</TableCell>
+              <TableCell>Humedad</TableCell>
+              <TableCell>Radiación</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>{item.temperature}</TableCell>
+                <TableCell>{item.humidity}</TableCell>
+                <TableCell>{item.radiation}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </CardActions>
     </Card>
   );
